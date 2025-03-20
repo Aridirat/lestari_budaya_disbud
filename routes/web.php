11acd7ffa147f476e58592f0080550792317dec4
Ida Bagus Adimas <aridirat@gmail.com>
2025-03-20 16:03:40 +0800

    Halaman home admin selesai

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.main');
 });
 
-Route::get('/index', function () {
-    return view('admin.index');
+Route::get('/odcb', function () {
+    return view('pages.budaya_odcb.index');
+});
+
+Route::get('/opk', function () {
+    return view('pages.budaya_opk.index');
+});
+
+Route::get('/kegiatan', function () {
+    return view('pages.kegiatan.index');
 });
