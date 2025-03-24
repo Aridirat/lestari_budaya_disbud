96c32c5a7d5c411df5a093ee7bef9ed843bef2ad
Ida Bagus Adimas <aridirat@gmail.com>
2025-03-24 08:34:12 +0800

    fitur login nich

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,26 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\IsLogin;
 
-Route::get('/', function () {
-    return view('layouts.main');
-});
+Route::get('/login',[AuthController::class, 'loginView']);
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/logout',[AuthController::class, 'logout']);
 
-Route::get('/odcb', function () {
-    return view('pages.budaya_odcb.index');
-});
+Route::middleware(IsLogin::class)->group(function(){
+    Route::get('/',[DashboardController::class, 'index']);
 
-Route::get('/opk', function () {
-    return view('pages.budaya_opk.index');
-});
-
-Route::get('/kegiatan', function () {
-    return view('pages.kegiatan.index');
+    Route::get('/odcb', function () {
+        return view('pages.budaya_odcb.index');
+    });
+    
+    Route::get('/opk', function () {
+        return view('pages.budaya_opk.index');
+    });
+    
+    Route::get('/kegiatan', function () {
+        return view('pages.kegiatan.index');
+    });
 });
